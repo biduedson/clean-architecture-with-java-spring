a0f5280fd75ab9f77279a14d47b7ec121d491aec
Edson <biduzao.bidu21@gmail.com>
2024-03-23 11:38:51 -0300

    feat: create user entity

---
+++ core/src/main/java/br/com/curso/core/domain/User.java
@@ -0,0 +1,143 @@
+package br.com.curso.core.domain;
+
+import br.com.curso.core.domain.enuns.UserTypeEnum;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+    private String emain;
+
+    private String password;
+    private  Taxnumber taxnumber;
+    private  String  fullname;
+   private UserTypeEnum type;
+   private TransactionPin transactionPin;
+   private LocalDateTime createAt;
+   private LocalDateTime updateAt;
+
+    public User(UUID id, String emain, String password, Taxnumber taxnumber, String fullname, UserTypeEnum type, TransactionPin transactionPin, LocalDateTime createAt, LocalDateTime updateAt) {
+        this.id = id;
+        this.emain = emain;
+        this.password = password;
+        this.taxnumber = taxnumber;
+        this.fullname = fullname;
+        this.type = type;
+        this.transactionPin = transactionPin;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+    }
+
+    public User(String emain, String password, Taxnumber taxnumber, String fullname, UserTypeEnum type, TransactionPin transactionPin) {
+        this.emain = emain;
+        this.password = password;
+        this.taxnumber = taxnumber;
+        this.fullname = fullname;
+        this.type = type;
+        this.transactionPin = transactionPin;
+    }
+
+    public User() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmain() {
+        return emain;
+    }
+
+    public void setEmain(String emain) {
+        this.emain = emain;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Taxnumber getTaxnumber() {
+        return taxnumber;
+    }
+
+    public void setTaxnumber(Taxnumber taxnumber) {
+        this.taxnumber = taxnumber;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public UserTypeEnum getType() {
+        return type;
+    }
+
+    public void setType(UserTypeEnum type) {
+        this.type = type;
+    }
+
+    public TransactionPin getTransactionPin() {
+        return transactionPin;
+    }
+
+    public void setTransactionPin(TransactionPin transactionPin) {
+        this.transactionPin = transactionPin;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+
+        if (!getId().equals(user.getId())) return false;
+        if (!getEmain().equals(user.getEmain())) return false;
+        if (!getPassword().equals(user.getPassword())) return false;
+        if (!getTaxnumber().equals(user.getTaxnumber())) return false;
+        if (!getFullname().equals(user.getFullname())) return false;
+        if (getType() != user.getType()) return false;
+        if (!getTransactionPin().equals(user.getTransactionPin())) return false;
+        if (!getCreateAt().equals(user.getCreateAt())) return false;
+        return getUpdateAt() != null ? getUpdateAt().equals(user.getUpdateAt()) : user.getUpdateAt() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId().hashCode();
+        result = 31 * result + getEmain().hashCode();
+        result = 31 * result + getPassword().hashCode();
+        result = 31 * result + getTaxnumber().hashCode();
+        result = 31 * result + getFullname().hashCode();
+        result = 31 * result + getType().hashCode();
+        result = 31 * result + getTransactionPin().hashCode();
+        result = 31 * result + getCreateAt().hashCode();
+        result = 31 * result + (getUpdateAt() != null ? getUpdateAt().hashCode() : 0);
+        return result;
+    }
+}
