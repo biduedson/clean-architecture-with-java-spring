9c5894111a3b1a14d25570c4ee8eca659695915d
Edson <biduzao.bidu21@gmail.com>
2024-03-26 15:10:31 -0300

    feat: add NotificationException

---
+++ core/src/main/java/br/com/curso/core/exception/NotificationException.java
@@ -0,0 +1,10 @@
+package br.com.curso.core.exception;
+
+public class NotificationException extends Exception{
+    private String code;
+
+    public NotificationException(String message, String code ) {
+        super(message);
+        this.code = code;
+    }
+}
