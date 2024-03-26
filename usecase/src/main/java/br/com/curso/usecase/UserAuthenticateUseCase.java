f5920631d4f2d772019af71576c1f2b3b6026bb4
Edson <biduzao.bidu21@gmail.com>
2024-03-26 15:09:27 -0300

    feat: add UserAuthenticateUseCase interface

--- usecase/src/main/java/br/com/curso/usecase/UserAuthenticateUseCase.java
+++ usecase/src/main/java/br/com/curso/usecase/UserAuthenticateUseCase.java
@@ -1,5 +1,7 @@
 package br.com.curso.usecase;
 
+import br.com.curso.core.exception.AuthenticateException;
+
 public interface UserAuthenticateUseCase {
-    boolean authenticate(String username, String password);
+    boolean authenticate(String username, String password) throws AuthenticateException;
 }
