d5279a556d3d9887d4995866d099fde0eeccddf0
Edson <biduzao.bidu21@gmail.com>
2024-03-26 15:08:56 -0300

    feat: add TransferUseCase interface

--- usecase/src/main/java/br/com/curso/usecase/TransferUseCase.java
+++ usecase/src/main/java/br/com/curso/usecase/TransferUseCase.java
@@ -1,7 +1,13 @@
 package br.com.curso.usecase;
 
 import br.com.curso.core.domain.Transaction;
+import br.com.curso.core.exception.InternalServerErrorException;
+import br.com.curso.core.exception.NotFoundException;
+import br.com.curso.core.exception.NotificationException;
+import br.com.curso.core.exception.TransferException;
+
+import java.math.BigDecimal;
 
 public interface TransferUseCase {
-    boolean transfer(Transaction transaction);
+    boolean transfer(String fromTaxNumber , String toTaxNumber, BigDecimal value) throws InternalServerErrorException, TransferException, NotFoundException, NotificationException;
 }
