43887e39da8005682ef7cbccfb51aaec576bd470
Edson <biduzao.bidu21@gmail.com>
2024-03-26 15:14:30 -0300

    feat: added more error codes to enums

--- core/src/main/java/br/com/curso/core/exception/enuns/ErrorCodeEnum.java
+++ core/src/main/java/br/com/curso/core/exception/enuns/ErrorCodeEnum.java
@@ -4,12 +4,20 @@ public enum ErrorCodeEnum {
     ON0001("Tax Number invalido.", "ON-0001"),
     ON0002("Tax Number indisponivel.", "ON-0002"),
     ON0003("E-mail indisponivel indisponivel.", "ON-0003"),
+    ON0004("Houve um erro na criação do usuario.", "ON-0004"),
 
-    TR0001("Usuário lojista não tem a função transferência disponiovel", "TR-0001"),
+    TR0001("Usuário lojista não tem a função transferência disponiovel.", "TR-0001"),
     TR0002("Saldo indisponivel", "TR-0002"),
-    TRP0001("Pin invalido","TRP-0001")
+    TRP0001("Pin invalido","TRP-0001"),
 
+    TR0003("Houve um erro ao fazer a transferencia.", "TR-0003"),
+
+    WA0001("Carteira não encontrada.", "WA-0001"),
+    NO0001("Houve  um erro na notificação do usuário.", "NO-0001"),
+    TR0004("Transferencia não autorizada.", "TR-0004"),
+    ATH0001("Houve  um erro na autenticação.", "AT-0001")
     ;
+
 
     private String message;
     private  String code;
