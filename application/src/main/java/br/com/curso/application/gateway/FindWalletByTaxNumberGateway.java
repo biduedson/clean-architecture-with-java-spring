9bfb6593a23c60dd24c65b31834476f4638ac558
Edson <biduzao.bidu21@gmail.com>
2024-03-26 15:06:57 -0300

    feat: add FindWalletByTaxNumberGateway interface

---
+++ application/src/main/java/br/com/curso/application/gateway/FindWalletByTaxNumberGateway.java
@@ -0,0 +1,8 @@
+package br.com.curso.application.gateway;
+
+import br.com.curso.core.domain.Wallet;
+
+public interface FindWalletByTaxNumberGateway {
+
+    Wallet findByTaxNumber(String taxNumber);
+}
