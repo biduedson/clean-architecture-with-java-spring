e4af3a8c3bf08e6dcde6118f34224f9afde0a816
Edson <biduzao.bidu21@gmail.com>
2024-03-26 21:00:37 -0300

    chore: add pin exception to transfer method

--- usecase/src/main/java/br/com/curso/usecase/TransferUseCase.java
+++ usecase/src/main/java/br/com/curso/usecase/TransferUseCase.java
@@ -1,13 +1,10 @@
 package br.com.curso.usecase;
 
 import br.com.curso.core.domain.Transaction;
-import br.com.curso.core.exception.InternalServerErrorException;
-import br.com.curso.core.exception.NotFoundException;
-import br.com.curso.core.exception.NotificationException;
-import br.com.curso.core.exception.TransferException;
+import br.com.curso.core.exception.*;
 
 import java.math.BigDecimal;
 
 public interface TransferUseCase {
-    boolean transfer(String fromTaxNumber , String toTaxNumber, BigDecimal value) throws InternalServerErrorException, TransferException, NotFoundException, NotificationException;
+    boolean transfer(String fromTaxNumber , String toTaxNumber, BigDecimal value, String pin) throws InternalServerErrorException, TransferException, NotFoundException, NotificationException, PinException;
 }
