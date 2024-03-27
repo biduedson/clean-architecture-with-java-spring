8927035e5b5d646d9872d091855606fbe0b12d93
Edson <biduzao.bidu21@gmail.com>
2024-03-26 21:04:38 -0300

    feat:  add CreateTransactionUseCase Implementation

---
+++ application/src/main/java/br/com/curso/application/usecaseImpl/CreateTransactionUseCaseImpl.java
@@ -0,0 +1,25 @@
+package br.com.curso.application.usecaseImpl;
+
+import br.com.curso.application.gateway.CreateTransactionGateway;
+import br.com.curso.core.domain.Transaction;
+import br.com.curso.core.exception.TransferException;
+import br.com.curso.core.exception.enuns.ErrorCodeEnum;
+import br.com.curso.usecase.CreateTransactionUseCase;
+
+public class CreateTransactionUseCaseImpl implements CreateTransactionUseCase {
+    private CreateTransactionGateway createTransactionGateway;
+
+
+    public CreateTransactionUseCaseImpl(CreateTransactionGateway createTransactionGateway) {
+        this.createTransactionGateway = createTransactionGateway;
+    }
+
+    @Override
+    public Transaction create(Transaction transaction) throws TransferException {
+        var transactionSaved = createTransactionGateway.create(transaction);
+        if(transactionSaved == null){
+            throw new TransferException(ErrorCodeEnum.TR0003.getMessage(), ErrorCodeEnum.TR0003.getCode());
+        }
+        return transactionSaved;
+    }
+}
