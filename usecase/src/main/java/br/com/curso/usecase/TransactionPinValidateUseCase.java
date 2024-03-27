ce4fb9027aecc859bfac3434a9c23399b8d35fce
Edson <biduzao.bidu21@gmail.com>
2024-03-27 16:52:31 -0300

    chore:  adding PinException to the method

--- usecase/src/main/java/br/com/curso/usecase/TransactionPinValidateUseCase.java
+++ usecase/src/main/java/br/com/curso/usecase/TransactionPinValidateUseCase.java
@@ -1,8 +1,9 @@
 package br.com.curso.usecase;
 
 import br.com.curso.core.domain.TransactionPin;
+import br.com.curso.core.exception.PinException;
 import br.com.curso.core.exception.TransferException;
 
 public interface TransactionPinValidateUseCase {
-    boolean validate(TransactionPin transactionPin) throws TransferException;
+    boolean validate(TransactionPin transactionPin) throws TransferException, PinException;
 }
