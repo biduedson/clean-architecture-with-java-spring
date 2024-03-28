80e2108af0fb929836d4f2ead04d3e1154b98a64
Edson <biduzao.bidu21@gmail.com>
2024-03-28 17:18:10 -0300

    feat: create TransactionEntity

---
+++ infrastructure/src/main/java/br/com/curso/infrastructure/entity/TransactionEntity.java
@@ -0,0 +1,45 @@
+package br.com.curso.infrastructure.entity;
+
+import br.com.curso.core.domain.enuns.TransactionStatusEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Transactions")
+public class TransactionEntity {
+    @Column(name = "Id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "FromWallet")
+    private WalletEntity fromWallet;
+
+    @ManyToOne
+    @JoinColumn(name = "ToWallet")
+    private WalletEntity toWallet;
+
+    @Column(name = "TransactionValue", nullable = false)
+    private BigDecimal value;
+
+    @Column(name = "Status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TransactionStatusEnum status;
+
+    @Column(name = "CreatedAt", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "UpdatedAt", nullable = false)
+    private LocalDateTime UpdatedAt;
+
+
+}
