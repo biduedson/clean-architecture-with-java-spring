f0b2b933f4e10da35d03159193e22caf6d3c4e1e
Edson <biduzao.bidu21@gmail.com>
2024-03-29 12:02:30 -0300

    refactor: excluding set transaction pin methods

--- core/src/main/java/br/com/curso/core/domain/User.java
+++ core/src/main/java/br/com/curso/core/domain/User.java
@@ -13,18 +13,16 @@ public class User {
     private  TaxNumber taxNumber;
     private  String  fullname;
    private UserTypeEnum type;
-   private TransactionPin transactionPin;
    private LocalDateTime createAt;
    private LocalDateTime updateAt;
 
-    public User(UUID id, String emain, String password, TaxNumber taxNumber, String fullname, UserTypeEnum type, TransactionPin transactionPin, LocalDateTime createAt, LocalDateTime updateAt) {
+    public User(UUID id, String emain, String password, TaxNumber taxNumber, String fullname, UserTypeEnum type, LocalDateTime createAt, LocalDateTime updateAt) {
         this.id = id;
         this.emain = emain;
         this.password = password;
         this.taxNumber = taxNumber;
         this.fullname = fullname;
         this.type = type;
-        this.transactionPin = transactionPin;
         this.createAt = createAt;
         this.updateAt = updateAt;
     }
@@ -35,7 +33,6 @@ public class User {
         this.taxNumber = taxNumber;
         this.fullname = fullname;
         this.type = type;
-        this.transactionPin = transactionPin;
     }
 
     public User() {
@@ -89,13 +86,6 @@ public class User {
         this.type = type;
     }
 
-    public TransactionPin getTransactionPin() {
-        return transactionPin;
-    }
-
-    public void setTransactionPin(TransactionPin transactionPin) {
-        this.transactionPin = transactionPin;
-    }
 
     public LocalDateTime getCreateAt() {
         return createAt;
@@ -122,7 +112,6 @@ public class User {
         if (!getTaxNumber().equals(user.getTaxNumber())) return false;
         if (!getFullname().equals(user.getFullname())) return false;
         if (getType() != user.getType()) return false;
-        if (!getTransactionPin().equals(user.getTransactionPin())) return false;
         if (!getCreateAt().equals(user.getCreateAt())) return false;
         return getUpdateAt() != null ? getUpdateAt().equals(user.getUpdateAt()) : user.getUpdateAt() == null;
     }
@@ -135,7 +124,6 @@ public class User {
         result = 31 * result + getTaxNumber().hashCode();
         result = 31 * result + getFullname().hashCode();
         result = 31 * result + getType().hashCode();
-        result = 31 * result + getTransactionPin().hashCode();
         result = 31 * result + getCreateAt().hashCode();
         result = 31 * result + (getUpdateAt() != null ? getUpdateAt().hashCode() : 0);
         return result;
