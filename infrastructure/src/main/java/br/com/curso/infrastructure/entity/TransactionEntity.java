adba2b7c8ca0a32467190690fb87c29eaf1b4a9c
Edson <biduzao.bidu21@gmail.com>
2024-03-29 12:00:36 -0300

    feat: add TransactionEntity

--- infrastructure/src/main/java/br/com/curso/infrastructure/entity/TransactionEntity.java
+++ infrastructure/src/main/java/br/com/curso/infrastructure/entity/TransactionEntity.java
@@ -38,7 +38,7 @@ public class TransactionEntity {
     @Column(name = "CreatedAt", nullable = false)
     private LocalDateTime createdAt;
 
-    @Column(name = "UpdatedAt", nullable = false)
+    @Column(name = "UpdatedAt")
     private LocalDateTime UpdatedAt;
 
 
