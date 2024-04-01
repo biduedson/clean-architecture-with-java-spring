58478e5db5f18cdc2c555901c6ed9438378bf021
Edson <biduzao.bidu21@gmail.com>
2024-04-01 11:15:50 -0300

    chore: added to user

--- infrastructure/src/main/java/br/com/curso/infrastructure/mapper/UserMapper.java
+++ infrastructure/src/main/java/br/com/curso/infrastructure/mapper/UserMapper.java
@@ -1,13 +1,19 @@
 package br.com.curso.infrastructure.mapper;
 
+import br.com.curso.core.domain.TaxNumber;
 import br.com.curso.core.domain.User;
+import br.com.curso.infrastructure.dto.request.CreateUserRequest;
 import br.com.curso.infrastructure.entity.UserEntity;
 import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Component
 public class UserMapper {
 
     public UserEntity toUserEntity(User user){
+
         return new UserEntity(
            user.getId(),
            user.getEmain(),
@@ -17,6 +23,17 @@ public class UserMapper {
            user.getType(),
            user.getCreateAt(),
            user.getUpdateAt()
+        );
+    }
+    public User toUser(CreateUserRequest request) throws Exception {
+        TaxNumber taxNumber =  new TaxNumber(request.taxNumber()) ;
+
+        return new User(
+                request.email(),
+                request.password(),
+                taxNumber,
+                request.fullName(),
+                request.type()
         );
     }
 }
