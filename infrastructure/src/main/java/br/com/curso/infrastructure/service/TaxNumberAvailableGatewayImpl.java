a83ab55b0ee2284bb3c7aa861b3e6380da496dab
Edson <biduzao.bidu21@gmail.com>
2024-04-01 11:17:27 -0300

    feat: added TaxNumberAvailableGateway Implementation

---
+++ infrastructure/src/main/java/br/com/curso/infrastructure/service/TaxNumberAvailableGatewayImpl.java
@@ -0,0 +1,23 @@
+package br.com.curso.infrastructure.service;
+
+import br.com.curso.application.gateway.TaxNumberAvailableGateway;
+import br.com.curso.infrastructure.repository.UserEntityRepository;
+import org.springframework.stereotype.Service;
+import static br.com.curso.infrastructure.utils.Utilities.log;
+
+
+@Service
+public class TaxNumberAvailableGatewayImpl implements TaxNumberAvailableGateway {
+    private UserEntityRepository userEntityRepository;
+
+    public TaxNumberAvailableGatewayImpl(UserEntityRepository userEntityRepository) {
+        this.userEntityRepository = userEntityRepository;
+    }
+
+    @Override
+    public Boolean taxNumberAvailable(String taxNumber) {
+        log.info("Inicio da verificação de TaxNumber disponível.");
+        return !userEntityRepository.existsByTaxNumber(taxNumber);
+
+    }
+}
