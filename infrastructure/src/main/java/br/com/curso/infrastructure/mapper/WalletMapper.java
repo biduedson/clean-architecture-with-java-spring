a2e0678d0ef8af787b0845065569fc1e50f183ba
Edson <biduzao.bidu21@gmail.com>
2024-04-01 13:37:59 -0300

    chore: added constructors and wallet mapper

--- infrastructure/src/main/java/br/com/curso/infrastructure/mapper/WalletMapper.java
+++ infrastructure/src/main/java/br/com/curso/infrastructure/mapper/WalletMapper.java
@@ -8,6 +8,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class WalletMapper {
+    private  TransactionPinMapper transactionPinMapper;
+    private  UserMapper userMapper;
+
+    public WalletMapper(TransactionPinMapper transactionPinMapper, UserMapper userMapper) {
+        this.transactionPinMapper = transactionPinMapper;
+        this.userMapper = userMapper;
+    }
+
     public WalletEntity towalletEntity(Wallet wallet, UserEntity userEntity, TransactionPinEntity transactionPinEntity){
         return new WalletEntity(
                 wallet.getBalance(),
@@ -16,6 +24,19 @@ public class WalletMapper {
                 wallet.getCreatedAt(),
                 wallet.getUpdatedAt()
 
+        );
+    }
+    public  Wallet toWallet(WalletEntity walletEntity) throws Exception {
+        if(walletEntity ==null){
+            return null;
+        }
+        return  new Wallet(
+             walletEntity.getId(),
+             transactionPinMapper.toTransactionPin(walletEntity.getTransactionPinEntity()),
+             walletEntity.getBalance(),
+             userMapper.toUser(walletEntity.getUserEntity()),
+             walletEntity.getCreatedAt(),
+             walletEntity.getUpdatedAt()
 
         );
     }
