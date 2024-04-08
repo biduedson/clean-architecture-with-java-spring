71f127b5b943469b23b0a7792495a88f897402b9
Edson <biduzao.bidu21@gmail.com>
2024-04-08 13:52:45 -0300

    feat: add TransactionGatewayImpl

---
+++ infrastructure/src/main/java/br/com/curso/infrastructure/service/CreateTransactionGatewayImpl.java
@@ -0,0 +1,28 @@
+package br.com.curso.infrastructure.service;
+
+import br.com.curso.application.gateway.CreateTransactionGateway;
+import br.com.curso.core.domain.Transaction;
+import br.com.curso.infrastructure.mapper.TransactionMapper;
+import br.com.curso.infrastructure.repository.TransactionEntityRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateTransactionGatewayImpl implements CreateTransactionGateway {
+    private TransactionEntityRepository transactionEntityRepository;
+    private TransactionMapper transactionMapper;
+
+    public CreateTransactionGatewayImpl(TransactionEntityRepository transactionEntityRepository, TransactionMapper transactionMapper) {
+        this.transactionEntityRepository = transactionEntityRepository;
+        this.transactionMapper = transactionMapper;
+    }
+
+    @Override
+    public Transaction create(Transaction transaction)throws Exception {
+        try{
+            var transactionEntity = transactionMapper.createTransaction(transaction);
+            return transactionMapper.toTransaction(transactionEntityRepository.save(transactionEntity));
+        }catch (Exception e){
+            return  null;
+        }
+    }
+}
